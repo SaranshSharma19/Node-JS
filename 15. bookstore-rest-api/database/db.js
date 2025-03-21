59acdeaf5c69266e445205a2b61c5d838b4d4919
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2025-03-21 16:52:36 +0530

    Removed MongoDB Credentials

--- 15. bookstore-rest-api/database/db.js
+++ 15. bookstore-rest-api/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectToDB = async() => {
     try{
-        await mongoose.connect('mongodb+srv://saranshsharma1919:saranshsharma1919@cluster0.rv1w7.mongodb.net/bookstore')
+        await mongoose.connect('********************************************************************')
         console.log('MongoDB Connected Succesfully');
     }catch(err){
         console.error('MongoDB Connection Failed', err)
